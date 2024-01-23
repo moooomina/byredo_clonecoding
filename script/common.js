279b7d8455fea63dc8628fe915174e14125ac691
mina <viva3927@google.com>
2024-01-23 18:32:55 +0900

    index페이지 디자인 완성

--- script/common.js
+++ script/common.js
@@ -1,5 +1,4 @@
 //nav 동적기능
-
 //메뉴 변수
 const gnb = document.querySelectorAll('#gnb li > a')
 console.log(gnb)
@@ -14,12 +13,55 @@ subWrap_func()
 gnb.forEach((t,i)=>{
     console.log(t,i)
     t.addEventListener('mouseover',()=>{
-        subWrap_func()
         subWrap[i].style.display='flex'
     })
-})
-gnb.forEach((t,i)=>{
     t.addEventListener('mouseout',()=>{
         subWrap[i].style.display='none'
     })
+})
+
+subWrap.forEach((obj)=>{
+    obj.addEventListener('mouseover',()=>{
+        obj.style.display='flex'
+    })
+    obj.addEventListener('mouseout',()=>{
+        obj.style.display='none'
+    })
+})
+
+//----------------------------------------------------
+//버튼 동적기능
+//아이콘 변수
+const icon = document.querySelectorAll('.icon')
+const search = document.querySelector('.search')
+const cart = document.querySelector('.cart')
+const close = document.querySelectorAll('.close')
+
+//보여지는 변수
+const searchFrm = document.querySelector('.searchFrm')
+const cartBg = document.querySelector('.cartBg')
+console.log(searchFrm,cartBg)
+
+searchFrm.classList.add('hide') /* 자바스크립트에서 추가한 클래스를 CSS에 꼭 적어줘야함 */
+search.addEventListener('click',()=>{
+    searchFrm.classList.toggle('hide')
+    searchFrm.classList.toggle('showFlex')
+})
+
+cartBg.classList.add('hide')
+cart.addEventListener('click',()=>{
+    cartBg.classList.toggle('hide')
+    cartBg.classList.toggle('showBlock')
+})
+
+//--------------------------------------------------
+//언어선택
+const langImg = document.querySelectorAll('.lang > a > img')
+const langSelect = document.querySelectorAll('.langSelect')
+console.log(langImg,langSelect)
+
+langSelect.classList.add('hide')
+langImg.addEventListener('click',()=>{
+    langSelect.classList.toggle('hide')
+    langSelect.classList.toggle('showFlex')
 })
